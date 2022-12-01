4ef43ced1b488e4e76d19af249a70a071771846f
SashaLawyer <alexbayb95@yandex.ru>
2022-12-01 12:50:48 +0300

    set app for windows

--- compile.sh
+++ compile.sh
@@ -7,7 +7,14 @@ mkdir -p build/gui/mac/amd/Tonutils\ Proxy.app/Contents/MacOS
 CGO_CFLAGS=-mmacosx-version-min=10.9 CGO_LDFLAGS=-mmacosx-version-min=10.9 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -o build/gui/mac/amd/Tonutils\ Proxy.app/Contents/MacOS/tonutils-proxy cmd/proxy-gui/main.go
 
 echo "Building WINDOWS GUI AMD"
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o build/tonutils-proxy-gui.exe cmd/proxy-gui/main.go
+p=$(pwd)
+# To start script need to declare your own LIBS env variable
+LIBS="C:\Users\alexb\libs"
+GO111MODULE="on" CGO_CXXFLAGS="-I$LIBS\webview2\build\native\include" CGO_LDFLAGS="-L$LIBS\webview2\build\native\x64" CGO_ENABLED=1 GOOS="windows" GOARCH="amd64"  go build -ldflags="-H windowsgui" -o build/tonutils-proxy-gui.exe cmd/proxy-gui/main.go
+rh.exe -open build/tonutils-proxy-gui.exe -save build/tonutils-proxy-gui.exe -action addskip -res build/ton_icon.ico -mask ICONGROUP,MAINICON,
+iscc /Q[p] "$p\build\win-install.iss"
+rm "$p\build\tonutils-proxy-gui.exe"
+
 echo "Building LINUX GUI AMD"
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o build/tonutils-proxy-gui cmd/proxy-gui/main.go
 
