355c0fdec185d794d2ecf0d137d9bdd1be2eeda1
Oleg Baranov <xssnick8@gmail.com>
2025-04-29 18:34:16 +0400

    Graceful stop for cli

--- cmd/proxy-cli/main.go
+++ cmd/proxy-cli/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/xssnick/tonutils-proxy/cmd/proxy-cli/config"
 	"github.com/xssnick/tonutils-proxy/proxy"
 	"os"
+	"os/signal"
 )
 
 var GitCommit string
@@ -45,9 +46,33 @@ func main() {
 		}
 	}
 
-	err = proxy.RunProxy(context.Background(), *addr, cfg.ADNLKey, nil, "CLI "+GitCommit, *blockHttp, *networkConfigPath, cfg.TunnelConfig, customTinNetCfg)
-	if err != nil {
-		log.Fatal().Err(err).Msg("proxy failed")
-		return
+	tunnelEnabled := cfg.TunnelConfig != nil && cfg.TunnelConfig.NodesPoolConfigPath != ""
+	closerCtx, stop := context.WithCancel(context.Background())
+
+	var tunnelCtx context.Context
+	if tunnelEnabled {
+		var cancel context.CancelFunc
+		tunnelCtx, cancel = context.WithCancel(context.Background())
+		proxy.OnTunnelStopped = cancel
 	}
+
+	go func() {
+		err = proxy.RunProxy(closerCtx, *addr, cfg.ADNLKey, nil, "CLI "+GitCommit, *blockHttp, *networkConfigPath, cfg.TunnelConfig, customTinNetCfg)
+		if err != nil {
+			log.Fatal().Err(err).Msg("proxy failed")
+			return
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	stop()
+
+	log.Info().Msg("Received interrupt signal, shutting down...")
+	if tunnelEnabled {
+		log.Info().Msg("Committing tunnel payments...")
+		<-tunnelCtx.Done()
+	}
+	log.Info().Msg("Shutdown complete")
 }
